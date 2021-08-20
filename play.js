5b7adc03536a97b9971c1b3358d9abf77605e554
Success-O <89156476+Success-O@users.noreply.github.com>
2021-08-20 12:36:16 +0100

    Create play.js

---
+++ play.js
@@ -0,0 +1,13 @@
+
+function biodata() {
+   let name = 'Success Ongharaka';
+   let nickname = 'Suc';
+   let age = '10 years old';
+   let stateOfOrigin = 'Delta State';
+   let gender = 'female';
+   let nationality = 'Nigerian';
+   let maritalStatus = 'Single';
+   console.log = ([name, nickname, age, stateOfOrigin, gender, nationality, maritalStatus]);
+   
+}    
+  biodata();
